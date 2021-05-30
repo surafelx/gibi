c895fd6016b7257a9837f3f9ecb883087ee368c0
Surafel Yimam <surafelyimam.sy@gmail.com>
2021-05-30 12:31:56 +0300

    Access Level Added

--- app/src/main/java/com/srfl/gibi/Common.java
+++ app/src/main/java/com/srfl/gibi/Common.java
@@ -37,6 +37,7 @@ public class Common extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_common);
+        String userId = fAuth.getInstance().getCurrentUser().getUid();
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -50,18 +51,49 @@ public class Common extends AppCompatActivity {
 //            }
 //        });
 
+
+
+
+        // TODO - Add Loading
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.getMenu().clear();
+        fStore = FirebaseFirestore.getInstance();
+        DocumentReference df = fStore.collection("Users").document(userId);
+        df.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if(documentSnapshot.getString("isRegistrar") != null){
+                    navigationView.inflateMenu(R.menu.activity_registrar_drawer);
+                } else if(documentSnapshot.getString("isStudent") != null){
+                    navigationView.inflateMenu(R.menu.activity_student_drawer);
+                }
+                else if(documentSnapshot.getString("isDepartment") != null){
+                    navigationView.inflateMenu(R.menu.activity_department_drawer);
+                }
+                else if(documentSnapshot.getString("isInstructor") != null){
+                    navigationView.inflateMenu(R.menu.activity_instructor_drawer);
+                }
+
+            }
+        });
+
+
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_student)
+                R.id.nav_home)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
     }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
